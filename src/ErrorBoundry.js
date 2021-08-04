82aeaac1ea0df4bd73eba303051261a716091c6c
dell <guptsb-cont@mahindra.com>
2021-08-04 08:58:40 +0530

    UpdaTE:

--- src/ErrorBoundry.js
+++ src/ErrorBoundry.js
@@ -6,13 +6,15 @@ export default class ErrorBoundary extends React.Component {
     }
   
     static getDerivedStateFromError(error) {
+        alert('Error Boundry')
       // Update state so the next render will show the fallback UI.
       return { hasError: true };
     }
   
     componentDidCatch(error, errorInfo) {
+        alert('Error Boundry')
       // You can also log the error to an error reporting service
-      logErrorToMyService(error, errorInfo);
+      //logErrorToMyService(error, errorInfo);
     }
   
     render() {
