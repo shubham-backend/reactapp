1bd376ca3775fd82edb132a907cc27a81d3a884a
dell <guptsb-cont@mahindra.com>
2021-08-04 08:56:44 +0530

    UpdaTE:

--- src/ErrorBoundry.js
+++ src/ErrorBoundry.js
@@ -1,5 +1,5 @@
 import React from "react"
-class ErrorBoundary extends React.Component {
+export default class ErrorBoundary extends React.Component {
     constructor(props) {
       super(props);
       this.state = { hasError: false };
